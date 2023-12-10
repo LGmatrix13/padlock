e70f73c6b436ab1c9a4b7d65266f13ba8baa61c2
Liam Grossman <liam.j.grossman@gmail.com>
2023-12-10 16:04:51 -0500

    Add location featur

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from functools import wraps
 from flask import Flask, session, render_template, redirect, url_for, flash, Response
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField
-from wtforms.fields import StringField, SubmitField, TextAreaField, SelectField, IntegerField
+from wtforms.fields import StringField, SubmitField, TextAreaField, SelectField, FloatField
 from wtforms.validators import InputRequired
 import crypto_backend as cb
 import pandas as pd
@@ -23,8 +23,8 @@ class ImportKeyForm(FlaskForm):
 
 class SendLocationForm(FlaskForm):
     contact = SelectField("Contacts")
-    longitude = IntegerField("Longitude")
-    latitude = IntegerField("Latitude")
+    longitude = FloatField("Longitude")
+    latitude = FloatField("Latitude")
     note = TextAreaField("Note", validators=[InputRequired()])
     submit = SubmitField("Submit")
 
@@ -53,7 +53,7 @@ class Locations:
             ciphertext
         ]
     def me(self, added_users: AddedUsers) -> pd.DataFrame:
-        temp = self.data[self.data['user_id'] == session['user_id']].merge(added_users.me(), on="contact_id")
+        temp = self.data[self.data['contact_id'] == session['user_id']].merge(added_users.me(), on="contact_id")
         return temp[['contact_id', 'name', 'ciphertext', 'nonce', 'sessionkey']]
         
 
@@ -118,12 +118,14 @@ def get_index():
     user = users.me()
     messages = locations.me(added_users).to_dict(orient="records")
     for message in messages:
-        message["plaintext"] = cb.decrypt_message_with_aes_and_rsa(
-            user.get("private"), 
-            b64decode(message.get('sessionkey'), validate=True),
-            b64decode(message.get('nonce'), validate=True),
-            b64decode(message.get('ciphertext'), validate=True)
-        ).decode('utf-8')
+        message["data"] = json.loads(
+            cb.decrypt_message_with_aes_and_rsa(
+                user.get("private"), 
+                b64decode(message.get('sessionkey'), validate=True),
+                b64decode(message.get('nonce'), validate=True),
+                b64decode(message.get('ciphertext'), validate=True)
+            ).decode('utf-8')
+        )
     return render_template('locations.html', locations = messages)
 
 
@@ -150,7 +152,7 @@ def post_add_contact():
 @added_user_required
 def get_send_location():
     form = SendLocationForm()
-    form.contact.choices = [(user["user_id"], user["name"]) for user in added_users.me().to_dict(orient="records")]
+    form.contact.choices = [(user["contact_id"], user["name"]) for user in added_users.me().to_dict(orient="records")]
     return render_template("send_location.html", form = form)
 
 @app.post("/send-location/")
@@ -158,13 +160,20 @@ def get_send_location():
 @added_user_required
 def post_send_location():
     form = SendLocationForm()
-    form.contact.choices = [(user['user_id'], user["name"]) for user in added_users.me().to_dict(orient="records")]
+    form.contact.choices = [(user['contact_id'], user["name"]) for user in added_users.me().to_dict(orient="records")]
+    print(form.contact.choices)
     if form.validate():
         contact_id = form.contact.data
-        note = form.note.data.encode('utf-8')
-        contacts = added_users.me()
-        contact = contacts[contacts['contact_id'] == contact_id].iloc[0].to_dict()
-        encrypted_session_key, nonce, ciphertext = cb.encrypt_message_with_aes_and_rsa(contact["public"], note)
+        data = {
+            "note": form.note.data,
+            "latitude": form.latitude.data,
+            "longitude": form.longitude.data
+        }
+        user = users.me()
+        encrypted_session_key, nonce, ciphertext = cb.encrypt_message_with_aes_and_rsa(
+            cb.rsa_deserialize_public_key(user["public"]),
+            json.dumps(data).encode('utf-8')
+        )
         sessionkey = b64encode(encrypted_session_key).decode('ascii')
         nonce = b64encode(nonce).decode('ascii')
         ciphertext = b64encode(ciphertext).decode('ascii')
